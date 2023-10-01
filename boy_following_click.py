3d2686743edc03594ebbc151dae260014789c79b
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-10-01 21:10:13 +0900

    add check duration and make move to clicked

--- boy_following_click.py
+++ boy_following_click.py
@@ -10,6 +10,7 @@ ch=load_image("animation_sheet.png")
 TUK_GROUND=load_image("TUK_GROUND.png")
 x,y=TUK_WIDTH//2,TUK_HEIGHT//2
 x0,y0=x,y
+x1,y1=x,y
 hand_x,hand_y=TUK_WIDTH//2,TUK_HEIGHT//2
 clicks=[]
 running=True
@@ -30,11 +31,28 @@ def handle_events():
 while(running):
     clear_canvas()
     TUK_GROUND.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
-    ch.clip_draw(frame*100,direction*100,100,100,x,y)
     hand.draw(hand_x,hand_y)
+    if x==x1 and y==y1:
+        if len(clicks)!=0:
+            x1,y1=clicks[0][0],clicks[0][1]
+            if x1>x:
+                direction=1
+            elif x1<x:
+                direction=0
+            if x1==x and y1==y:
+                x0,y0=x,y
+                del clicks[0]
+        else:
+            ch.clip_draw(0,direction*100+200,100,100,x,y)
+    else:
+        ch.clip_draw(frame*100,direction*100,100,100,x,y)
+        per=1/num_of_moves
+        x,y=(1-per*t)*x0+per*t*x1,(1-per*t)*y0+per*t*y1
+        t=(t+1)%(num_of_moves+1)
+        frame=(frame+1)%8
     for click in clicks:
         hand.draw(click[0],click[1])
-    handle_events()
     update_canvas()
+    handle_events()
     delay(0.03)
 close_canvas()
